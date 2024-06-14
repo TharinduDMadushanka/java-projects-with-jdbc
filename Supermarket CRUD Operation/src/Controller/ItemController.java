29351c96939b509f639f237813b602da3dfcdc39
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-14 14:34:12 +0530

    first commit

--- Supermarket CRUD Operation/src/Controller/ItemController.java
+++ Supermarket CRUD Operation/src/Controller/ItemController.java
@@ -1,5 +1,34 @@
 package Controller;
 
+import Model.ItemModel;
+
 public class ItemController {
+    private ItemModel itemModel;
+
+    public ItemController() throws Exception{
+        this.itemModel = new ItemModel();
+    }
+
+    public String saveItem(ItemDto itemDto) throws Exception{
+        String resp = itemModel.saveItem(itemDto);
+        return resp;
+    }
+
+    public ArrayList<ItemDto> getAllItem() throws Exception{
+        ArrayList<ItemDto> itemDtos = itemModel.getAllItem();
+        return itemDtos;
+    }
+    public ItemDto searchItem(String itemId)throws Exception{
+        ItemDto itemDto =itemModel.getItem(itemId);
+        return itemDto;
+    }
+    public String deleteItem(String itemCode) throws Exception{
+        String resp = itemModel.deleteItem(itemCode);
+        return resp;
+    }
+    public String updateItem(ItemDto itemDto)throws Exception{
+        String resp = itemModel.updateItem(itemDto);
+        return resp;
+    }
 
 }
