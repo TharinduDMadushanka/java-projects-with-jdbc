b79a75ce3a98e72d6dc2ed9fdaa107acd412a42b
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-14 08:49:22 +0530

    first commit

--- Supermarket Management System/src/View/CreateEmployee.java
+++ Supermarket Management System/src/View/CreateEmployee.java
@@ -1,7 +1,15 @@
 package View;
 
+import Model.Database;
+import Model.Employee;
 import Model.Option;
+
+import java.util.Scanner;
 
 
 public class CreateEmployee implements Option {
+    @Override
+    public void oper(Employee user, Scanner s, Database database) {
+
+    }
 }
