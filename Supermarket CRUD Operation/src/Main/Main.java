7feca6ca22a8893f07404cfc9a801367a0b24652
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-14 14:32:34 +0530

    first commit

--- Supermarket CRUD Operation/src/Main/Main.java
+++ Supermarket CRUD Operation/src/Main/Main.java
@@ -1,7 +1,10 @@
 package Main;
 
+import View.CustomerView;
+import View.ItemView;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new ItemView().setVisible(true);
         new CustomerView().setVisible(true);
     }
