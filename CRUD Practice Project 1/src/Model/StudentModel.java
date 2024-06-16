9e85f5b40a72cab13970d4f49df5c60b7790a8b7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-16 19:52:55 +0530

    first commit

--- CRUD Practice Project 1/src/Model/StudentModel.java
+++ CRUD Practice Project 1/src/Model/StudentModel.java
@@ -5,10 +5,13 @@
 package Model;
 
 import Dto.StudentDto;
+import com.mysql.cj.protocol.Resultset;
 import db.DBConnection;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.sql.ResultSet;
 /**
  *
  * @author ASUS
@@ -32,6 +35,22 @@ public class StudentModel {
          statement.setInt(3,studentDto.getGrade());
         
          return statement.executeUpdate() >0 ? "Success" : "Fail";
+    }
+    
+    public ArrayList<StudentDto> getAllStudent() throws  Exception{
+    
+        String sql="GET * FROM student";       
+        PreparedStatement statement=connection.prepareStatement(sql);
+        ResultSet rst= statement.executeQuery();
+        
+        ArrayList<StudentDto> studentDtos= new ArrayList();
+        
+        while(rst.next()){
+        
+            StudentDto dto = new StudentDto(rst.getString("name"),rst.getInt("age"),rst.getInt("grade"));
+            studentDtos.add(dto);
+        }
+        return studentDtos;
     }
     
 //    public String deleteStudent(){
