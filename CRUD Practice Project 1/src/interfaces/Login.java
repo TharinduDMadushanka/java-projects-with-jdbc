ea34c1c4291f570236573014d50f1672555ec9c9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-16 10:06:51 +0530

    first commit

--- CRUD Practice Project 1/src/interfaces/Login.java
+++ CRUD Practice Project 1/src/interfaces/Login.java
@@ -75,6 +75,7 @@ public class Login extends javax.swing.JFrame {
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
         
         MainFrame mainFrame=new MainFrame();
+        mainFrame.setVisible(true);
         
     }//GEN-LAST:event_btnLoginActionPerformed
 
