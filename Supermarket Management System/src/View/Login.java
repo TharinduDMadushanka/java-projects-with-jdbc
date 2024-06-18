a4eddee6725a8175dbec761f28c90539dcd993bc
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-18 10:41:19 +0530

    first commit

---
+++ Supermarket Management System/src/View/Login.java
@@ -0,0 +1,18 @@
+package View;
+
+import Model.Database;
+import Model.Employee;
+import Model.Option;
+
+import java.util.Scanner;
+
+public class Login implements Option {
+    @Override
+    public void oper(Employee user, Scanner s, Database database) {
+        System.out.println("Welcome to supermarket management system.");
+        System.out.print("Enter your email : ");
+        String email = s.next();
+        System.out.print("Enter your password : ");
+        String password = s.next();
+    }
+}
--- Supermarket Management System/src/supermarket/Main.java
+++ Supermarket Management System/src/supermarket/Main.java
@@ -3,22 +3,27 @@ package supermarket;
 import Controller.CreateEmployee;
 import Model.Database;
 import Model.Employee;
+import View.Login;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Employee admin = new Employee("Admin", "Perera", "123@gmail.com", "077 3152079", "Galle", 50000, "123") {
-                @Override
-                public int getDepartment() {
-                    return 0;
-                }
-            };
-            Database database = new Database();
-            new CreateEmployee(admin, database);
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        new Login().oper(null, new Scanner(System.in),new Database());
     }
 }
+
+//try {
+//Employee admin = new Employee("Admin", "Perera", "123@gmail.com", "077 3152079", "Galle", 50000, "123") {
+//    @Override
+//    public int getDepartment() {
+//        return 0;
+//    }
+//};
+//Database database = new Database();
+//            new CreateEmployee(admin, database);
+//        } catch (SQLException | ClassNotFoundException e) {
+//        e.printStackTrace();
+//        }
