578289e98c70907277a7dc6385ed915c788d7096
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-18 10:03:58 +0530

    first commit

--- Supermarket Management System/src/Controller/CreateEmployee.java
+++ Supermarket Management System/src/Controller/CreateEmployee.java
@@ -2,10 +2,12 @@ package Controller;
 
 import Model.Database;
 import Model.Employee;
-import Model.Option;
-
-
-import java.util.Scanner;
 
 public class CreateEmployee {
+
+    public CreateEmployee(Employee e , Database database) {
+
+        String insert="INSERT INTO employee VALUES(?,?,?,?,?,?,?,?)";
+
+    }
 }
