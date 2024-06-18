390f31a3344af41c9d54eaf84488d53775cb9e8e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-18 10:48:05 +0530

    first commit

--- Supermarket Management System/src/View/Login.java
+++ Supermarket Management System/src/View/Login.java
@@ -3,16 +3,17 @@ package View;
 import Model.Database;
 import Model.Employee;
 import Model.Option;
-
+import java.lang.ModuleLayer.Controller;
 import java.util.Scanner;
 
-public class Login implements Option {
-    @Override
-    public void oper(Employee user, Scanner s, Database database) {
-        System.out.println("Welcome to supermarket management system.");
-        System.out.print("Enter your email : ");
-        String email = s.next();
-        System.out.print("Enter your password : ");
-        String password = s.next();
-    }
-}
+//public class Login implements Option {
+//    @Override
+//    public void oper(Employee user, Scanner s, Database database) {
+//        System.out.println("Welcome to supermarket management system.");
+//        System.out.print("Enter your email : ");
+//        String email = s.next();
+//        System.out.print("Enter your password : ");
+//        String password = s.next();
+//        Login login = new Login(email,password);
+//    }
+//}
