400e367d14184ba17c976a502364d5fc56040953
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-20 19:38:28 +0530

    add ordedto

--- Supermarket CRUD Operation/src/Dto/OrderDto.java
+++ Supermarket CRUD Operation/src/Dto/OrderDto.java
@@ -1,4 +1,50 @@
 package Dto;
 
 public class OrderDto {
+
+    private String orderId;
+    private String custId;
+    private String date;
+
+    public OrderDto() {
+    }
+
+    public OrderDto(String orderId, String custId, String date) {
+        this.orderId = orderId;
+        this.custId = custId;
+        this.date = date;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDto{" +
+                "orderId='" + orderId + '\'' +
+                ", custId='" + custId + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
 }
