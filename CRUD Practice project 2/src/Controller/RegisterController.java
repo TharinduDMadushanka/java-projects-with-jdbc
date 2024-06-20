9287fd7cc9fe0be0fc2df76239554263e2f0a97c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-20 09:15:17 +0530

    reg controller

---
+++ CRUD Practice project 2/src/Controller/RegisterController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import Model.RegisterModel;
+
+public class RegisterController {
+    private RegisterModel registerModel;
+
+    public RegisterController()throws Exception {
+        this.registerModel = new RegisterModel();
+    }
+}
