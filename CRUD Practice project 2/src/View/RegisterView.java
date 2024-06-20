6b5aed86b4315e6d474c99299744105e42c25fbd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-20 10:03:19 +0530

    reg view

--- CRUD Practice project 2/src/View/RegisterView.java
+++ CRUD Practice project 2/src/View/RegisterView.java
@@ -267,7 +267,11 @@ public class RegisterView extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new RegisterView().setVisible(true);
+                try {
+                    new RegisterView().setVisible(true);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
