b4121a14b4f0ac3544fb82be8d703a1eabdf008c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-20 09:01:32 +0530

    db connection

--- CRUD Practice project 2/src/Db/DBConnection.java
+++ CRUD Practice project 2/src/Db/DBConnection.java
@@ -1,9 +1,17 @@
 package Db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
 
     private static DBConnection dbConnection;
     private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection= DriverManager.getConnection("jdbc.mysql://localhost:3306/project2","root","Thariya920@");
+
+    }
 }
