04d57ac14b395f9573676ec43c89161f643924b2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-20 09:05:15 +0530

    reg model

--- CRUD Practice project 2/src/Model/RegisterModel.java
+++ CRUD Practice project 2/src/Model/RegisterModel.java
@@ -1,4 +1,15 @@
 package Model;
 
+import Db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class RegisterModel {
+    private final Connection connection;
+
+    public RegisterModel() throws ClassNotFoundException, SQLException {
+        this.connection= DBConnection.getInstance().getConnection();
+    }
+
 }
