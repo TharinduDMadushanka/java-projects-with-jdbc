0a5b4aa0b660bcec2163a9cd50f5a482acfae723
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-21 10:27:52 +0530

    Order Controller

--- Supermarket CRUD Operation/src/Controller/OrderController.java
+++ Supermarket CRUD Operation/src/Controller/OrderController.java
@@ -1,6 +1,10 @@
 package Controller;
 
+import Dto.OrderDetailDto;
+import Dto.OrderDto;
 import Model.OrderModel;
+
+import java.util.ArrayList;
 
 public class OrderController {
 
@@ -8,6 +12,11 @@ public class OrderController {
 
     public OrderController()throws Exception {
         orderModel = new OrderModel();
+    }
+
+    public String placeOrder(OrderDto orderDto, ArrayList<OrderDetailDto> orderDetailDtos)throws Exception {
+
+        return orderModel.placeOrder(orderDto, orderDetailDtos);
     }
 
 }
