60527d9dd0cce68347012583bfca62cb896d2aaa
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-21 10:26:38 +0530

    Order Controller

--- Supermarket CRUD Operation/src/Controller/OrderController.java
+++ Supermarket CRUD Operation/src/Controller/OrderController.java
@@ -1,4 +1,13 @@
 package Controller;
 
+import Model.OrderModel;
+
 public class OrderController {
+
+    private OrderModel orderModel;
+
+    public OrderController()throws Exception {
+        orderModel = new OrderModel();
+    }
+
 }
