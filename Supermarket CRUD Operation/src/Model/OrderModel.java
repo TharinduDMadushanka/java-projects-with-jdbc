d53ab81769f5dc6043c7b9ea4affded8de89d4ba
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-21 10:13:50 +0530

    OrderModel class

--- Supermarket CRUD Operation/src/Model/OrderModel.java
+++ Supermarket CRUD Operation/src/Model/OrderModel.java
@@ -3,8 +3,10 @@ package Model;
 import DB.DBConnection;
 import Dto.OrderDetailDto;
 import Dto.OrderDto;
+import com.sun.source.tree.BreakTree;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -20,8 +22,24 @@ public class OrderModel {
 
         try {
             connection.setAutoCommit(false); // disable auto save
+
+            String orderSql="INSERT INTO orders VALUES (?,?,?)";
+            PreparedStatement orderStatement = connection.prepareStatement(orderSql);
+            orderStatement.setString(1, orderDto.getOrderId());
+            orderStatement.setString(2,orderDto.getDate());
+            orderStatement.setString(3,orderDto.getCustId());
+
+            boolean isOrderSaved=orderStatement.executeUpdate()>0;// true
+
+
+
+
+        } catch (Exception e) {
+            connection.rollback();// when disable program roll back all data
+            e.printStackTrace();
+        } finally {
+            connection.setAutoCommit(true);
         }
-
+        return "Success all";
     }
-
 }
